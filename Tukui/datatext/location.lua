49203545c0ccc07d2009de8a2a7d91cec33d9d21
Flippy <jhogan@orcon.net.nz>
2010-08-22 17:44:52 +1200

    fixed loction data text

--- Tukui/datatext/location.lua
+++ Tukui/datatext/location.lua
@@ -1,38 +1,54 @@
---------------------------------------------------------------------
- -- LOCATION by Foof, edited for v11 by Toresh
---------------------------------------------------------------------
 if TukuiCF["datatext"].location and TukuiCF["datatext"].location > 0 then
 	local Stat = CreateFrame("Frame")
+	Stat:SetFrameStrata("HIGH")
 	Stat:EnableMouse(true)
-	Stat:SetFrameStrata("BACKGROUND")
-	Stat:SetFrameLevel(3)
 	
-	local Text  = TukuiInfoLeft:CreateFontString(nil, "OVERLAY")
+	local Text  = TukuiInfoLeft:CreateFontString(nil, "LOW")
 	Text:SetFont(TukuiCF.media.font, TukuiCF["datatext"].fontsize)
 	TukuiDB.PP(TukuiCF["datatext"].location, Text)
- 
-	local function OnEvent(self, event)
-		local location = GetRealZoneText()
-		local pvpType, isFFA, zonePVPStatus = GetZonePVPInfo()
-		
-		if (pvpType == "sanctuary") then
-			location = "|cff69C9EF"..location.."|r" -- light blue
-		elseif (pvpType == "friendly") then
-			location = "|cff00ff00"..location.."|r" -- green
-		elseif (pvpType == "contested") then
-			location = "|cffffff00"..location.."|r" -- yellow
-		elseif (pvpType == "hostile" or pvpType == "combat" or pvpType == "arena" or not pvpType) then
-			location = "|cffff0000"..location.."|r" -- red
-		else
-			location = location -- white
+
+	local int = 1
+	local function Update(self, t)
+		int = int - t
+		if int < 0 then
+			local zoneName = GetRealZoneText()
+			local pvpType, isSubZonePvP, factionName = GetZonePVPInfo()
+
+			if pvpType == "sanctuary" then
+				Text:SetTextColor(.41, .8, .94)
+			elseif pvpType == "arena" then
+				Text:SetTextColor(1, .1, .1)
+			elseif pvpType == "friendly" then
+				Text:SetTextColor(.1, 1, .1)
+			elseif pvpType == "hostile" then
+				Text:SetTextColor(1, .1, .1)
+			elseif pvpType == "contested" then
+				Text:SetTextColor(1, .7, 0)
+			elseif pvpType == "combat" then
+				Text:SetTextColor(1, .1, .1)
+			elseif pvpType == nil then
+				Text:SetTextColor(1, 1, 1)
+			end
+			Text:SetText(zoneName)
+			int = 1
 		end
 		
-		Text:SetText(location)
-		--UIFrameFadeIn(TukuiLocationPanel, 0.5, 0, 1)
+		self:SetAllPoints(Text)
+		self:SetScript("OnEnter", function()
+			local x, y = GetPlayerMapPosition("player");
+			local coords = format("%.1f - %.1f", x*100, y*100)
+			GameTooltip:SetOwner(this, "ANCHOR_TOP");
+			GameTooltip:ClearAllPoints()
+			GameTooltip:SetPoint("BOTTOM", self, "TOP", 0, TukuiDB.mult)
+			GameTooltip:ClearLines()
+			--GameTooltip:AddDoubleLine(cStart .. "Sub Zone: " .. cEnd, GetMinimapZoneText(), _, _, _, 1, 1, 1)
+			--GameTooltip:AddDoubleLine(cStart .. "Coordinates: " .. cEnd, coords, _, _, _, 1, 1, 1)
+			GameTooltip:Show()
+		end)
+		self:SetScript("OnLeave", function() GameTooltip:Hide() end)
 	end
-	Stat:RegisterEvent("PLAYER_ENTERING_WORLD")
-	Stat:RegisterEvent("ZONE_CHANGED_NEW_AREA")
+	
+	Stat:SetScript("OnUpdate", Update)
 	Stat:SetScript("OnMouseDown", function() ToggleFrame(WorldMapFrame) end)
-	Stat:SetScript("OnEvent", OnEvent)
-
+	Update(Stat, 10)
 end
