398f03321acc6446d8b86fcd19144076d7acb418
Toresh <jhogan@orcon.net.nz>
2010-10-13 23:42:20 +1300

    right bars done

--- Tukui/actionbars/Bar4.lua
+++ Tukui/actionbars/Bar4.lua
@@ -3,6 +3,7 @@ if not TukuiCF["actionbar"].enable == true then return end
 ---------------------------------------------------------------------------
 -- setup MultiBarRight as bar #4
 ---------------------------------------------------------------------------
+--first right bar (done)
 
 local TukuiBar4 = CreateFrame("Frame","TukuiBar4",UIParent) -- bottomrightbar
 TukuiBar4:SetAllPoints(TukuiActionBarBackground)
@@ -13,9 +14,17 @@ for i= 1, 12 do
 	local b2 = _G["MultiBarRightButton"..i-1]
 	b:ClearAllPoints()
 	if i == 1 then
-		b:SetPoint("TOPRIGHT", TukuiActionBarBackgroundRight, "TOPRIGHT", TukuiDB.Scale(-4), TukuiDB.Scale(-4))
+		if TukuiCF["actionbar"].horizonbars == true then
+			b:SetPoint("TOPLEFT", TukuiActionBarBackgroundRight, "TOPLEFT", TukuiDB.Scale(4), TukuiDB.Scale(-4))
+		else	
+			b:SetPoint("TOPRIGHT", TukuiActionBarBackgroundRight, "TOPRIGHT", TukuiDB.Scale(-4), TukuiDB.Scale(-4))
+		end
 	else
-		b:SetPoint("TOP", b2, "BOTTOM", 0, -TukuiDB.buttonspacing)
+		if TukuiCF["actionbar"].horizonbars == true then
+			b:SetPoint("LEFT", b2, "RIGHT", TukuiDB.buttonspacing, 0)
+		else
+			b:SetPoint("TOP", b2, "BOTTOM", 0, -TukuiDB.buttonspacing)
+		end
 	end	
 end
 
